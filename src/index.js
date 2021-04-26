e5e650e36d347bce592c5a0fe1a5e2f5cc6de59a
dh.no <dh.no@naviworks.com>
2021-04-26 13:33:10 +0900

    Add Potato

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+
+// 여기서 모든 요소를 그린다.
+// 렌더에는 하나의 js만 들어갈 수있다!!
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+); 
